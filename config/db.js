ef9591b56e7f8390723d7cb510426bf6ed5af6e9
Ajayi Segun <ajayisegun2003@gmail.com>
2024-08-27 20:35:28 +0100

    mongodbcommit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const URL =
-  "mongodb+srv://ajayisegun2003:mDT098kRhA2AplUG@cluster0.f7bkt.mongodb.net/";
+  "mongodb+srv://ajayisegun2003:mDT098kRhA2AplUG@cluster0.f7bkt.mongodb.net/TodoBackend?retryWrites=true&w=majority";
 
 export const dbConfig = async () => {
   // Connect to MongoDB
